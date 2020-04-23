526a8d196a62679a3688fa61da3586a487b0db8a
Vlad Stavarache <vlad.stavarache@ro.ibm.com>
2020-04-23 15:05:15 +0300

    move shop data and collections in own reducers, update corresponding components with new redux flow

---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,21 @@
+import { createSelector } from 'reselect';
+
+const selectShop = (state) => state.shop;
+
+export const selectShopCollections = createSelector(
+  [selectShop],
+  (shop) => shop.collections
+);
+
+export const selectCollectionForPreview = createSelector(
+  [selectShopCollections],
+  (collections) => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectCollection = collectionUrlParam => 
+  createSelector(
+    [selectShopCollections],
+    (collections) => {
+      return collections[collectionUrlParam];
+    }
+)
